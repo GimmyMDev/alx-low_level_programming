92a4732cc39e36437844da7e2d4d3d82e566ce1d
Ntombikayise <gimmymtshali@gmail.com>
2022-09-13 12:46:57 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,13 @@
 /**
  * _islower - function that checks for lowercase character
  *
- * Return: 1 if it's lowercase
- * Return: 0 if otherwise
+ * Return: 1 if lowercase, else 0 if otherwise
  */
 
-int islower(int c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }

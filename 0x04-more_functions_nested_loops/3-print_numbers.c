f3aa0e7c3fe788f1718196d588ba5dd18439b4ed
Ntombikayise <gimmymtshali@gmail.com>
2022-09-15 13:00:22 -0700

    3. Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,11 +10,9 @@ void print_numbers(void)
 {
 	int i = 0;		
 		
-	while (i <= 9)				
-	{						
-		_putchar(i + '0');						
-		++i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
 	}
-	_putchar('\n');
-				
+	_putchar('\n');			
 }

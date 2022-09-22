49cf119d7ae996581ef6fe0b5003efa0611f6083
Ntombikayise <gimmymtshali@gmail.com>
2022-09-22 11:54:07 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strcat - function to concatenate two strings
+ *
+ * @dest: Output string
+ * @src: input strings
+ *
+ * Return: Concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+		for (b = 0; src[b] != '\0'; b++)
+		{
+			dest[a] = src[b];
+			a++;
+		}
+		return (dest);
+	}
+}
